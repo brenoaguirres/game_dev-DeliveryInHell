771470482267a59baaf237c0be7ac46b22eca4f1
brenoaguirres <bennyaguirres1998@gmail.com>
2024-10-11 12:44:08 -0300

    +: DialogueBox behavior improved.

--- Assets/Scripts/DialogueSystem/DialogueBox.cs
+++ Assets/Scripts/DialogueSystem/DialogueBox.cs
@@ -1,16 +1,69 @@
 using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.Serialization;
 
-public class DialogueBox : MonoBehaviour
+namespace CBPXL.DialogueSystem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class DialogueBox : MonoBehaviour
     {
+        #region FIELDS
+        [SerializeField] private TextMeshProUGUI _textBox;
+        [SerializeField] private string[] _lines;
+        [SerializeField] private float _typeSpeed;
+        [SerializeField] private int _maxLines;
+        private int _lineIndex;
+        private int _boxLineIndex;
+        #endregion
+        void Start()
+        {
+            InitializeDialogue();
+        }
         
-    }
+        #region BEHAVIOR METHODS
+        private void InitializeDialogue()
+        {
+            _textBox.text = string.Empty;
+            _lineIndex = 0;
+            _boxLineIndex = 0;
+            StartCoroutine(TypeLine());
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private IEnumerator TypeLine()
+        {
+            if (_boxLineIndex > _maxLines - 1)
+            {
+                _textBox.text = string.Empty;
+                yield return new WaitForSeconds(_typeSpeed);
+            }
+            else if (_boxLineIndex < 0)
+            {
+                _textBox.text += "\n";
+            }
+            
+            foreach (char c in _lines[_lineIndex].ToCharArray())
+            {
+                _textBox.text += c;
+                yield return new WaitForSeconds(_typeSpeed);
+            }
+            
+            NextLine();
+        }
+
+        private void NextLine()
+        {
+            if (_lineIndex < _lines.Length - 1)
+            {
+                _lineIndex++;
+                _boxLineIndex++;
+                StartCoroutine(TypeLine());
+            }
+            else
+            {
+                StopAllCoroutines();
+                gameObject.SetActive(false);
+            }
+        }
+        #endregion
     }
 }
